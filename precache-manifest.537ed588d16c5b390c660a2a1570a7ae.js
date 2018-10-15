c2b3fffcb5663a447f16ec006f422e22a911f618
Ivan Ivanov <ivan.milchev.ivanov@gmail>
2018-10-15 17:55:16 +0300

    Updates

---
+++ precache-manifest.537ed588d16c5b390c660a2a1570a7ae.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "71d94d3e81c14c9938cd3cc6f962d265",
+    "url": "/static/media/logo.71d94d3e.svg"
+  },
+  {
+    "revision": "aace22e1c679ea2f0adc",
+    "url": "/static/css/main.fcf50d77.chunk.css"
+  },
+  {
+    "revision": "63180369da881d7587e1a11d99c142b4",
+    "url": "/static/media/question.63180369.png"
+  },
+  {
+    "revision": "de302fc5c93bf0bfc27b",
+    "url": "/static/js/1.de302fc5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad299419083b3017d8b8860413f40e0f",
+    "url": "/static/media/category-cryptocurrency-atm.ad299419.png"
+  },
+  {
+    "revision": "01601bf1c887f8e2ecaf932d188b7349",
+    "url": "/static/media/star.01601bf1.png"
+  },
+  {
+    "revision": "b0b37955f47dc08e93e1d114c7276829",
+    "url": "/static/media/chart-area.b0b37955.png"
+  },
+  {
+    "revision": "a4b719857b49976a5cae088390246efe",
+    "url": "/static/media/history.a4b71985.png"
+  },
+  {
+    "revision": "439ae51b854a3cfae8f8449b88ba14ff",
+    "url": "/static/media/questionMark.439ae51b.svg"
+  },
+  {
+    "revision": "aace22e1c679ea2f0adc",
+    "url": "/static/js/main.aace22e1.chunk.js"
+  },
+  {
+    "revision": "625899e03c3436271ec382edeb3713d1",
+    "url": "/static/media/chart-line.625899e0.png"
+  },
+  {
+    "revision": "25ecec155ad30906aa890736c5f83934",
+    "url": "/static/media/chart-pie.25ecec15.png"
+  },
+  {
+    "revision": "c3cdb69e09678a1e692faf2448a7cd88",
+    "url": "/static/media/location.c3cdb69e.png"
+  },
+  {
+    "revision": "5f4050dbfe0fbbc2bd40789ad33d3092",
+    "url": "/static/media/Location-icon.5f4050db.svg"
+  },
+  {
+    "revision": "bc5fcd6f8ec28b6bc210643bac38052a",
+    "url": "/static/media/search.bc5fcd6f.png"
+  },
+  {
+    "revision": "8dd3de039c7729700f43dd70442916a9",
+    "url": "/static/media/plus.8dd3de03.png"
+  },
+  {
+    "revision": "fe8e9538e1012e2db39a573b6e75c7aa",
+    "url": "/static/media/cubes.fe8e9538.png"
+  },
+  {
+    "revision": "7f39f5484597d1379d0abcc493b090fa",
+    "url": "/static/media/exchange-alt.7f39f548.png"
+  },
+  {
+    "revision": "de302fc5c93bf0bfc27b",
+    "url": "/static/css/1.f4776787.chunk.css"
+  },
+  {
+    "revision": "d33b0382d41deb356b3ef88ecba1e0c7",
+    "url": "/index.html"
+  }
+];
